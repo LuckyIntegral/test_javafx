62fd20363af1087b6e93c73221baf6588bee448e
Vitalik <frantsv2004@gmail.com>
2023-04-01 22:45:15 +0200

    first commit

---
+++ src/main/java/com/example/testfx/HelloApplication.java
@@ -0,0 +1,18 @@
+package com.example.testfx;
+
+import com.example.testfx.config.AppConfig;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) {
+        AppConfig.init(stage, this.getClass());
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
